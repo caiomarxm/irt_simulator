2859ab6ee17a937c83241421959df373440a8fea
Caio Martins <icaiomarxm@gmail.com>
2024-04-30 00:05:42 -0300

    feat: adding user creation on repository

--- backend/src/repositories/user.py
+++ backend/src/repositories/user.py
@@ -1,3 +1,19 @@
-from fastapi import Depends
-from core.db import get_session
+from sqlmodel import Session
+from core.security import hash_password
 
+from models.user import (
+    User,
+    UserBase,
+    UserCreate,
+    UserUpdate
+)
+
+
+def create_user(user_in: UserCreate, session: Session) -> None:
+    user_in = User.model_validate(
+        user_in, update={"hashed_password": hash_password(password=user_in.password)}
+        )
+    session.add(user_in)
+    session.commit()
+    session.refresh(user_in)
+    return user_in
