8009e3eb66a70f8e298576f9c030b3f562c913af
Caio Martins <icaiomarxm@gmail.com>
2024-04-29 23:44:49 -0300

    feat: user models

---
+++ backend/src/models/user.py
@@ -0,0 +1,23 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional, Any
+
+
+class UserBase(SQLModel):
+    email: str = Field(unique=True, index=True)
+    full_name: str
+    is_active: bool = True
+    is_superuser: bool = False
+
+
+# Properties to receive on user creation
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(UserBase):
+    email: Optional[str] = None
+    password: Optional[str] = None
+
+
+class User(UserBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
