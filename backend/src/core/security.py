8d8ef0c8129ae116f7cbca88d6e783d6c184c2fc
Caio Martins <icaiomarxm@gmail.com>
2024-05-02 20:11:24 -0300

    feat: verify password

--- backend/src/core/security.py
+++ backend/src/core/security.py
@@ -5,3 +5,7 @@ pwd_encrypter = CryptContext(schemes=['bcrypt'], deprecated="auto")
 
 def hash_password(password: str) -> str:
     return pwd_encrypter.hash(password)
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return pwd_encrypter.verify(password, hashed_password)
