f23d0eb5617daf5f2e9ae61af4ba8a4899f3c060
Caio Martins <icaiomarxm@gmail.com>
2024-05-03 19:11:57 -0300

    feat: adding QueryClientProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+
+const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
