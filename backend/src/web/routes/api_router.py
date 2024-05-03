4af93335f18091cf6b2e2b60533cf41d8b240ada
Caio Martins <icaiomarxm@gmail.com>
2024-05-03 19:10:19 -0300

    feat: users route

--- backend/src/web/routes/api_router.py
+++ backend/src/web/routes/api_router.py
@@ -2,7 +2,8 @@ from fastapi.routing import APIRouter
 
 from web.routes import (
     health,
-    login
+    login,
+    users
     )
 
 
@@ -10,3 +11,4 @@ api_router = APIRouter()
 
 api_router.include_router(health.router, prefix="/health", tags=["health"])
 api_router.include_router(login.router, prefix="/login", tags=["login"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
--- backend/src/web/routes/users.py
+++ backend/src/web/routes/users.py
@@ -1,9 +1,12 @@
 from fastapi.routing import APIRouter
 
+from core.deps import InjectCurrentUser
+
 from models.user import (
     User,
     UserCreate,
-    UserUpdate
+    UserUpdate,
+    UserPublic
 )
 
 
@@ -16,6 +19,11 @@ def list_users():
 
 def get_user_by_id(id: int):
     ...
+
+
+@router.get("/me", response_model=UserPublic)
+def read_user_me(current_user: InjectCurrentUser):
+    return current_user
 
 
 def create_user():
