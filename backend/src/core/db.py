23978a746519e4220708c65d48596febcd714961
Caio Martins <icaiomarxm@gmail.com>
2024-05-03 19:08:49 -0300

    feat: moving init_db routine to init_db.py

--- backend/src/core/db.py
+++ backend/src/core/db.py
@@ -1,28 +1,6 @@
-from sqlmodel import Session, create_engine, select
-from typing import Generator
+from sqlmodel import create_engine
 
 from core.settings import settings
-from models import User, UserCreate
-from repositories.user import create_user
 
 
 engine = create_engine(str(settings.DATABASE_URI))
-
-def init_db(session: Session) -> None:
-    from sqlmodel import SQLModel
-    SQLModel.metadata.create_all(engine)
-
-    first_user = session.exec(
-        select(User).where(User.email == settings.FIRST_SUPERUSER_EMAIL)
-    ).first()
-
-    if not first_user:
-        user_in = UserCreate(
-            email=settings.FIRST_SUPERUSER_EMAIL,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            full_name="Admin User",
-            is_superuser=True
-        )
-
-        # TO-DO: create user
-        create_user(user_in=user_in, session=session)
--- backend/src/init_db.py
+++ backend/src/init_db.py
@@ -1,11 +1,35 @@
 import logging
-from sqlmodel import Session
+from sqlmodel import Session, select
 
-from core.db import engine, init_db
+from core.db import engine
+from core.settings import settings
+
+from models.user import User, UserCreate
+
+from repositories.user import create_user
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+def init_db(session: Session) -> None:
+    from sqlmodel import SQLModel
+    SQLModel.metadata.create_all(engine)
+
+    first_user = session.exec(
+        select(User).where(User.email == settings.FIRST_SUPERUSER_EMAIL)
+    ).first()
+
+    if not first_user:
+        user_in = UserCreate(
+            email=settings.FIRST_SUPERUSER_EMAIL,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            full_name="Admin User",
+            is_superuser=True
+        )
+
+        # TO-DO: create user
+        create_user(user_in=user_in, session=session)
 
 
 def init() -> None:
