c8e96b4d19ed25dbd0c653a8047a5de1d56bb5fb
Caio Martins <icaiomarxm@gmail.com>
2024-05-04 17:19:53 -0300

    feat: adding info and quizz tabs

---
+++ frontend/src/pages/ExamPage.tsx
@@ -0,0 +1,39 @@
+import { Button, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
+import { Quizz } from "../components/quizz/Quizz";
+import { useEffect, useState } from "react";
+
+export const ExamPage = () => {
+  const [isOpenForSubmission, setIsOpenForSubmission] = useState<boolean>(false)
+
+  // TODO: useEffect to see if the quizz is currently available to submissions
+  useEffect(() => {
+    setIsOpenForSubmission(true)
+  }, [])
+
+  // TODO: fetch if user already took the quizz and submitted any anwers
+
+  return (
+    <>
+      <Button onClick={() => {setIsOpenForSubmission(!isOpenForSubmission)}}>Change Open (Temporary)</Button>
+      <Tabs>
+        <TabList>
+          <Tab>Current Exam</Tab>
+          <Tab>Take the Quizz!</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Text>
+              Some informational data about the quizz here
+            </Text>
+            { isOpenForSubmission ? <Text>Looks like the quizz is open for submission!</Text> : <></> }
+          </TabPanel>
+
+          <TabPanel>
+            <Quizz isOpenForSubmition={isOpenForSubmission} />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
+  );
+};
