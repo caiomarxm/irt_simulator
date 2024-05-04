93b057c8403f48344b723ff2a9b1e795f9d0796b
Caio Martins <icaiomarxm@gmail.com>
2024-05-04 16:20:37 -0300

    feat: adding sidebar and navbar

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,5 +1,7 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { Sidebar } from "../components/Sidebar";
+import { Navbar } from "../components/Navbar";
 
 const RootLayout = () => {
   return (
@@ -10,12 +12,15 @@ const RootLayout = () => {
         bg="blue.600"
         minHeight={{ lg: "100vh" }}
         p={{ base: "20px", lg: "30px" }}
-      ></GridItem>
+      >
+        <Sidebar />
+      </GridItem>
       <GridItem
         as="main"
         colSpan={{ base: 6, lg: 4, xl: 5 }}
         p="40px"
       >
+        <Navbar />
         <Outlet />
       </GridItem>
     </Grid>
