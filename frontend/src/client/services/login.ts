7287bcbc4dece1d1a77b7ae7321dd1babf62e9be
Caio Martins <icaiomarxm@gmail.com>
2024-05-04 10:07:31 -0300

    feat: loginAccessToken

--- frontend/src/client/services/login.ts
+++ frontend/src/client/services/login.ts
@@ -1,21 +1,22 @@
-import { httpClient } from "../core/api";
-import { AccessTokenRequestBody } from "../models/token";
+import { httpClient } from "../core/httpClient";
+import {
+  AccessTokenRequestBody,
+  AccessTokenResponseBody,
+} from "../models/token";
 
-export type TDataLoginAccessToken = {
+export type LoginFormData = {
   formData: AccessTokenRequestBody;
 };
 
 export class LoginService {
-  public static async loginAccessToken(data: TDataLoginAccessToken) {
+  public static async loginAccessToken(
+    data: LoginFormData
+  ): Promise<AccessTokenResponseBody> {
 
-    return httpClient.postForm(
-        "/login/token",
-        data,
-        {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        }
-    )
+    return httpClient.postForm("/login/token", data, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
   }
 }
