ab8325ab78fc92849c04dd7016c330025e2f407b
Caio Martins <icaiomarxm@gmail.com>
2024-05-04 17:20:16 -0300

    feat: adding dummy admin page

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,5 +1,20 @@
+import { Button } from "@chakra-ui/react";
+import { UserService } from "../client/services/userService";
+import useAuth from "../hooks/useAuth";
+
 export const Admin = () => {
+  const handleClick = async () => {
+    const user = await UserService.readUserMe()
+    alert(JSON.stringify(user));
+  };
+
+  const { performLogout } = useAuth()
+
   return (
-    <div>Admin</div>
-  )
-}
+    <>
+      <div>Admin</div>
+      <Button onClick={handleClick}>Test User</Button>
+      <Button onClick={performLogout}>Logout</Button>
+    </>
+  );
+};
