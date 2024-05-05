5d8f77ebd87a177d40949eef1a978728724c27fc
Caio Martins <icaiomarxm@gmail.com>
2024-05-05 14:43:27 -0300

    feat: adding currentExam hook

---
+++ frontend/src/hooks/useCurrentExam.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { ExamService } from "../client/services/examService";
+import { useState } from "react";
+
+export const useCurrentExam = () => {
+  const [currentYear, setCurrentYear] = useState<number>(
+    new Date().getFullYear()
+  );
+
+  const query = useQuery({
+    queryKey: ["list-exams"],
+    queryFn: async () => await ExamService.listExams(currentYear, true),
+  });
+
+  const [isOpenForSubmission] = useState<boolean>(
+    !query.data?.data.is_closed ?? false
+  )
+
+  return {
+    ...query,
+    data: query.data?.data,
+    setCurrentYear,
+    isOpenForSubmission
+  };
+};
