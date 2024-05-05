da6eb7bb7c01c9edc739e4a6e554db74be2b187f
Caio Martins <icaiomarxm@gmail.com>
2024-05-05 11:50:55 -0300

    feat: adding update by id and delete operations

--- backend/src/repositories/exams.py
+++ backend/src/repositories/exams.py
@@ -5,8 +5,10 @@ from models.exams import Exam, ExamCreate, ExamUpdate
 
 def create_exam(exam: ExamCreate, session: Session) -> Exam:
     exam_in = Exam.model_validate(
-        exam_in
+        exam
         )
+
+    # TODO: Check if year is valid and if there's already an exam for that year
 
     session.add(exam_in)
     session.commit()
@@ -33,3 +35,19 @@ def update_exam(updated_exam: ExamUpdate, session: Session, exam: Exam) -> Exam:
     session.commit()
     session.refresh(exam)
     return exam
+
+
+def update_exam_by_id(id: int, updated_exam: ExamUpdate, session: Session) -> Exam:
+    exam = session.get(Exam, id)
+    return update_exam(updated_exam=updated_exam, session=session, exam=exam)
+
+
+def delete_exam(id: int, session: Session) -> Exam:
+    exam = session.get(Exam, id)
+
+    if exam:
+        session.delete(exam)
+        session.commit()
+        return True
+    
+    return False
