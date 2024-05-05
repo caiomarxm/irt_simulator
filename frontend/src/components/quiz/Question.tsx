ff4d624ba9067b58cecb75a0f6182e6c0b59e7da
Caio Martins <icaiomarxm@gmail.com>
2024-05-05 15:38:34 -0300

    feat: updating answers from child component

--- frontend/src/components/quiz/Question.tsx
+++ frontend/src/components/quiz/Question.tsx
@@ -5,26 +5,50 @@ import {
   RadioGroup,
   VStack,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 
-type Props = {
+export type QuestionProps = {
   index: number;
+  questionId: number;
   question: string;
   options: string[];
+  answers: Record<string, any>[];
+  setAnswers: Dispatch<SetStateAction<Record<string, any>[]>>;
 };
 
-export const Question = ({ index, question, options }: Props) => {
-  const [value, setValue] = useState<string>("")
+export const Question = ({
+  index,
+  question,
+  questionId: question_id,
+  options,
+  answers,
+  setAnswers,
+}: QuestionProps) => {
+  const [value, setValue] = useState<string>("");
+
+  const handleChange = (value: string) => {
+    setValue(value);
+    const entry = { question_id: question_id, response_index: parseInt(value) };
+    const newAnswers = answers.filter((answer) => answer.question_id != question_id);
+    newAnswers.push(entry);
+    setAnswers(newAnswers);
+  };
 
   const optionsList = options.map((option, i) => {
-    return <Radio key={i} value={`${i}`}>{option}</Radio>;
+    return (
+      <Radio key={i} value={`${i}`}>
+        {option}
+      </Radio>
+    );
   });
 
   return (
     <FormControl>
       <FormLabel as="legend">{`${index}) ${question}`}</FormLabel>
-      <RadioGroup mb={10} onChange={setValue} value={value}>
-        <VStack spacing={5} alignItems="left">{optionsList}</VStack>
+      <RadioGroup mb={10} onChange={handleChange} value={value}>
+        <VStack spacing={5} alignItems="left">
+          {optionsList}
+        </VStack>
       </RadioGroup>
     </FormControl>
   );
