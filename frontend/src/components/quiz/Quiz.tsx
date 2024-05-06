be41ba8fba86a90336a4a6ece268309963fb5877
Caio Martins <icaiomarxm@gmail.com>
2024-05-06 03:04:49 -0300

    feat: submit and save the quiz

--- frontend/src/components/quiz/Quiz.tsx
+++ frontend/src/components/quiz/Quiz.tsx
@@ -1,30 +1,108 @@
-import { Box, Button, Divider, HStack, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  HStack,
+  Heading,
+  Text,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import { Form } from "react-router-dom";
 import { Question } from "./Question";
 import { IQuestion } from "../../client/models/question";
-import { FormEvent, FormEventHandler, MouseEventHandler, useState } from "react";
+import {
+  FormEvent,
+  FormEventHandler,
+  MouseEventHandler,
+  useEffect,
+  useState,
+} from "react";
+import { IAnswer } from "../../client/models/answer";
+import { ISubmissionPost as ISubmissionPostData } from "../../client/models/submission";
+import { SubmissionService } from "../../client/services/submissionService";
+import { UserPublic } from "../../client/models/user";
+import ConfirmationModal from "./ConfirmationModal";
+import { CheckCircleIcon } from "@chakra-ui/icons";
 
 type QuizProps = {
-  isOpenForSubmition: boolean,
-  questions?: IQuestion[]
-}
+  isOpenForSubmition: boolean;
+  questions?: IQuestion[];
+  currentYear: number;
+};
 
-export const Quiz = ({ isOpenForSubmition, questions }: QuizProps) => {
+export const Quiz = ({
+  isOpenForSubmition,
+  questions,
+  currentYear,
+}: QuizProps) => {
+  // States used by the Quizz
+  const [answers, setAnswers] = useState([] as IAnswer[]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const handleSave: MouseEventHandler<HTMLButtonElement> = async (event: Event) => {
-    const userDataString = localStorage.getItem("userData")
-    let userData
+  // Hooks
+  const toast = useToast();
+
+  // Use Effect to render previous answers and if submit was commited!!!
+  useEffect(() => {}, []);
+
+  // Submit and Save handlers
+  const saveAnswers = async (submission: ISubmissionPostData) => {
+    const result = await SubmissionService.submitExam(submission);
+
+    return result;
+  };
+
+  const generateSubmissionPostData = (isCommited: boolean = false) => {
+    const userDataString = localStorage.getItem("userData");
+    let userData;
     if (userDataString) {
-      userData = JSON.parse(userDataString)
+      userData = JSON.parse(userDataString) as UserPublic;
     }
-  }
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event: FormEvent) => {
-    event.preventDefault()
+    return {
+      exam_year: currentYear,
+      user_id: userData?.id,
+      is_commited: isCommited,
+      answers: answers,
+    } as ISubmissionPostData;
+  };
 
-  }
+  const handleClickOnSave: MouseEventHandler<HTMLButtonElement> = async () => {
+    const submissionPostData = generateSubmissionPostData(false);
+    const result = await saveAnswers(submissionPostData);
 
-  const [answers, setAnswers] = useState([] as Record<string,any>[])
+    if ([200, 201].includes(result.status)) {
+      toast({
+        colorScheme: "green",
+        description: `Your quiz was saved successfully, you can come back and finish it later!`,
+        title: `Congratulations!`,
+        position: "top",
+        icon: <CheckCircleIcon />,
+      });
+    }
+  };
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (
+    event: FormEvent
+  ) => {
+    event.preventDefault();
+
+    onOpen();
+
+    // const submissionPostData = generateSubmissionPostData(true);
+    // const result = await saveAnswers(submissionPostData);
+
+    // if ([200, 201].includes(result.status)) {
+    //   toast({
+    //     colorScheme: "green",
+    //     description: `You've submitted your quiz successfully and your results will be available any time.`,
+    //     title: `Congratulations!`
+    //   })
+    // }
+  };
+
+  // Building the component
 
   if (!isOpenForSubmition) {
     return (
@@ -34,7 +112,7 @@ export const Quiz = ({ isOpenForSubmition, questions }: QuizProps) => {
         </Heading>
         <Text>Looks like the quiz isn't available for submissions yet</Text>
       </Box>
-    )
+    );
   }
 
   return (
@@ -59,8 +137,15 @@ export const Quiz = ({ isOpenForSubmition, questions }: QuizProps) => {
           />
         ))}
         <HStack spacing={5}>
-          <Button colorScheme="green" type="submit">Submit</Button>
-          <Button onClick={handleSave}>Save</Button>
+          <Button colorScheme="green" type="submit">
+            Submit
+          </Button>
+          <ConfirmationModal
+            isOpen={isOpen}
+            onClose={onClose}
+            onOpen={onOpen}
+          />
+          <Button onClick={handleClickOnSave}>Save</Button>
         </HStack>
       </Form>
     </Box>
