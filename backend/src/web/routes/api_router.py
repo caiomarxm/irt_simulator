1936c1078b8249d3d2268516fb39d4b02160c8f2
Caio Martins <icaiomarxm@gmail.com>
2024-05-05 23:16:17 -0300

    feat: submissions route

--- backend/src/web/routes/api_router.py
+++ backend/src/web/routes/api_router.py
@@ -1,10 +1,13 @@
+from fastapi import Depends
 from fastapi.routing import APIRouter
 
+from core.deps import get_current_user_from_token
 from controllers import (
     health,
     login,
     users,
-    exams
+    exams,
+    submissions
 )
 
 
@@ -14,3 +17,4 @@ api_router.include_router(health.router, prefix="/health", tags=["health"])
 api_router.include_router(login.router, prefix="/login", tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(exams.router, prefix="/exams", tags=["exams"])
+api_router.include_router(submissions.router, prefix="/submissions", tags=["submissions"], dependencies=[Depends(get_current_user_from_token)])
