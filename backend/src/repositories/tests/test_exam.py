c19f1674364f348e9fdbe645b07a8a87e5077f94
Caio Martins <icaiomarxm@gmail.com>
2024-05-07 12:15:37 -0300

    update: changing repo function names

--- backend/src/repositories/tests/test_exam.py
+++ backend/src/repositories/tests/test_exam.py
@@ -4,23 +4,21 @@ from sqlmodel import select, Session, text
 from core.db import engine
 from models.exams import (Exam, ExamCreate, ExamUpdate)
 from repositories.exams import (
-    get_exam_by_id,
-    get_exam_by_year,
-    create_exam,
-    update_exam,
+    read_exam_by_id,
+    read_exam_by_year,
     update_exam_by_id
     )
 
 
 def test_get_exam_by_id():
     with Session(engine) as session:
-        exam = get_exam_by_id(1, session=session)
+        exam = read_exam_by_id(1, session=session)
         assert isinstance(exam, (Exam, None))
 
 
 def test_get_exam_by_year():
     with Session(engine) as session:
-        exam = get_exam_by_year(2024, session=session)
+        exam = read_exam_by_year(2024, session=session)
         assert isinstance(exam, (Exam, None))
 
 
