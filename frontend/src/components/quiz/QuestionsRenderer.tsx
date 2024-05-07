6c824534f85687ba07da6412c9e7b02450236cc7
Caio Martins <icaiomarxm@gmail.com>
2024-05-07 01:27:12 -0300

    refactor: questions map to QuestionsRenderer

---
+++ frontend/src/components/quiz/QuestionsRenderer.tsx
@@ -0,0 +1,35 @@
+import { Dispatch } from "react";
+import { IAnswer } from "../../client/models/answer";
+import { IQuestion } from "../../client/models/question";
+import { Question } from "./Question";
+
+interface QuestionRendererProps {
+  questions: IQuestion[];
+  answers: IAnswer[];
+  setAnswers: Dispatch<React.SetStateAction<IAnswer[]>>;
+  isCommitted: boolean | undefined;
+}
+
+export const QuestionsRenderer = ({
+  questions,
+  answers,
+  setAnswers,
+  isCommitted,
+}: QuestionRendererProps) => {
+  return (
+    <>
+      {questions.map((item, i) => (
+        <Question
+          key={item.id}
+          index={i + 1}
+          questionId={item.id}
+          question={item.question}
+          options={item.options}
+          answers={answers}
+          setAnswers={setAnswers}
+          isCommitted={isCommitted}
+        />
+      ))}
+    </>
+  );
+};
