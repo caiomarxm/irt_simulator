c1b4939f247a7a6a3e249afba1526fc04922a4f4
Caio Martins <icaiomarxm@gmail.com>
2024-05-07 10:33:31 -0300

    feat: ignoring superuser as default

--- frontend/src/client/services/submissionService.ts
+++ frontend/src/client/services/submissionService.ts
@@ -3,22 +3,23 @@ import { httpClient } from "../core/httpClient";
 import { ISubmission, ISubmissionPost } from "../models/submission";
 
 export class SubmissionService {
-  public static listSubmissions(year: number|null = null, includeAnswers: boolean = false) : AxiosPromise<ISubmission[]> {
-    let filters: string = `include_answers=${includeAnswers}`
+  public static listSubmissions(
+    year: number | null = null,
+    includeAnswers: boolean = false,
+    ignoreSuperuser: boolean = true
+  ): AxiosPromise<ISubmission[]> {
+    let filters: string = `ignore_superuser=${ignoreSuperuser}&include_answers=${includeAnswers}`;
 
     if (year) {
-      filters += `&year=${year}`
+      filters += `&year=${year}`;
     }
 
-    return httpClient.get(
-        `/submissions?${filters}`
-    )
+    return httpClient.get(`/submissions?${filters}`);
   }
 
-  public static submitExam(submission: ISubmissionPost) : AxiosPromise<ISubmission> {
-    return httpClient.post(
-        `/submissions/submit`,
-        submission
-    )
+  public static submitExam(
+    submission: ISubmissionPost
+  ): AxiosPromise<ISubmission> {
+    return httpClient.post(`/submissions/submit`, submission);
   }
 }
