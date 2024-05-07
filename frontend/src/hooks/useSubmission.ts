feec2a23fc633a2235622ea28b045031430bbb55
Caio Martins <icaiomarxm@gmail.com>
2024-05-07 00:42:44 -0300

    feat: useSubmission hook

---
+++ frontend/src/hooks/useSubmission.ts
@@ -0,0 +1,30 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { SubmissionService } from "../client/services/submissionService";
+import { ISubmissionPost } from "../client/models/submission";
+
+export const useSubmission = () => {
+  const queryClient = useQueryClient();
+
+  const [currentYear] = useState<number>(new Date().getFullYear());
+
+  const query = useQuery({
+    queryKey: ["submissions"],
+    queryFn: () => SubmissionService.listSubmissions(currentYear, true),
+  });
+
+  const submissionMutation = useMutation({
+    mutationFn: async (submission: ISubmissionPost) => {
+      await SubmissionService.submitExam(submission);
+      queryClient.invalidateQueries({queryKey: ["submissions"]});
+    },
+  });
+
+  return {
+    ...query,
+    isLoading: query.isLoading,
+    refetch: query.refetch,
+    data: query.data?.data[0],
+    submissionMutation,
+  };
+};
