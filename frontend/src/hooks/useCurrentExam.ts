d4046bba8934396c70de6d3d6cea687f66631862
Caio Martins <icaiomarxm@gmail.com>
2024-05-08 05:28:52 -0300

    feat: exam mutations

--- frontend/src/hooks/useCurrentExam.ts
+++ frontend/src/hooks/useCurrentExam.ts
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { ExamService } from "../client/services/examService";
 import { useState } from "react";
 
 export const useCurrentExam = () => {
+  const queryClient = useQueryClient()
+
   const [currentYear, setCurrentYear] = useState<number>(
     new Date().getFullYear()
   );
@@ -12,10 +14,38 @@ export const useCurrentExam = () => {
     queryFn: async () => await ExamService.listExams(currentYear, true),
   });
 
+  const openExamMutation = useMutation({
+    mutationFn: async (id: number) => {
+      const result = await ExamService.openExam(id)
+      queryClient.invalidateQueries({ queryKey: ["list-exams"] });
+      return result
+    },
+  });
+
+  const closeExamMutation = useMutation({
+    mutationFn: async (id: number) => {
+      const result = await ExamService.closeExam(id)
+      queryClient.invalidateQueries({ queryKey: ["list-exams"] });
+      return result
+    },
+  });
+
+  const commitExamMutation = useMutation({
+    mutationFn: async (id: number) => {
+      const result = await ExamService.commitExam(id)
+      queryClient.invalidateQueries({ queryKey: ["list-exams"] });
+      return result
+    },
+  });
+
+
   return {
     ...query,
     data: query.data?.data,
     setCurrentYear,
-    currentYear
+    currentYear,
+    openExamMutation,
+    closeExamMutation,
+    commitExamMutation
   };
 };
