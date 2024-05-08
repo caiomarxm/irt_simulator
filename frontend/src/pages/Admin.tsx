82ca101131785ad44831bffd8452565b705379af
Caio Martins <icaiomarxm@gmail.com>
2024-05-08 05:28:31 -0300

    feat: manage exams page

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,20 +1,68 @@
-import { Button } from "@chakra-ui/react";
-import { UserService } from "../client/services/userService";
-import useAuth from "../hooks/useAuth";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spinner,
+} from "@chakra-ui/react";
+import { colors } from "../theme";
+import { useCurrentExam } from "../hooks/useCurrentExam";
 
 export const Admin = () => {
-  const handleClick = async () => {
-    const user = await UserService.readUserMe()
-    alert(JSON.stringify(user));
+  const {
+    data,
+    isLoading,
+    openExamMutation,
+    closeExamMutation,
+    commitExamMutation,
+  } = useCurrentExam();
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  const handleCommitExam = async () => {
+    const result = await commitExamMutation.mutateAsync(data?.id as number);
+    return result;
   };
 
-  const { performLogout } = useAuth()
+  const handleOpenExam = async () => {
+    const result = await openExamMutation.mutateAsync(data?.id as number);
+    return result;
+  };
+
+  const handleCloseExam = async () => {
+    const result = await closeExamMutation.mutateAsync(data?.id as number);
+    return result;
+  };
 
   return (
-    <>
-      <div>Admin</div>
-      <Button onClick={handleClick}>Test User</Button>
-      <Button onClick={performLogout}>Logout</Button>
-    </>
+    <Box>
+      <Menu>
+        <MenuButton textAlign="left">
+          <Card
+            display="block"
+            borderTop={`10px solid ${colors.background}`}
+            maxW="250px"
+          >
+            <CardHeader fontSize="1.2rem">Exam - {data?.year}</CardHeader>
+            <CardBody>{`Currently ${data?.is_closed ? "closed" : "open"} and ${data?.is_committed? "commited" : "not commited"}.`}</CardBody>
+            <Divider color="gray.200" />
+            <CardFooter>Click to manage the ongoing exam</CardFooter>
+          </Card>
+        </MenuButton>
+        <MenuList>
+          <MenuItem onClick={handleOpenExam}>Open Exam</MenuItem>
+          <MenuItem onClick={handleCloseExam}>Close Exam</MenuItem>
+          <MenuItem onClick={handleCommitExam}>Commit Exam</MenuItem>
+        </MenuList>
+      </Menu>
+    </Box>
   );
 };
