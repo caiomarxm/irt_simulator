0b225e4f65a70e8c63c02e0ef58993324c416d18
Caio Martins <icaiomarxm@gmail.com>
2024-05-08 03:45:22 -0300

    feat: rendering results

--- frontend/src/pages/ResultsPage.tsx
+++ frontend/src/pages/ResultsPage.tsx
@@ -1,5 +1,56 @@
+import {
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Heading,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import { useSubmission } from "../hooks/useSubmission";
+import { colors } from "../theme";
+
 export const ResultsPage = () => {
+  const { data, isLoading } = useSubmission();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (!data?.result) {
+    return (
+      <Box ml="10px">
+        <Heading as="h3" fontSize="1.2rem" mb={5}>
+          Exam Results
+        </Heading>
+        <Text>Sorry, it seems the results are not available yet.</Text>
+        <Text>You will be notified as soon as the results are available.</Text>
+      </Box>
+    );
+  }
+
   return (
-    <div>Results</div>
-  )
-}
+    <Box ml="10px">
+      <Heading as="h3" fontSize="1.2rem" mb={5}>
+        Exam Results
+      </Heading>
+      <Text mb={5}>The exam results are available, check your grade bellow.</Text>
+      <Card
+        display="block"
+        borderTop={`10px solid ${colors.background}`}
+        maxW="250px"
+      >
+        <CardHeader fontSize="1.2rem">Your Grade</CardHeader>
+        <CardBody>
+          <Heading>{data?.result}</Heading> out of 1000
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          {data?.result > 600 ? "You did very well!" : "Maybe next year..."}
+        </CardFooter>
+      </Card>
+    </Box>
+  );
+};
