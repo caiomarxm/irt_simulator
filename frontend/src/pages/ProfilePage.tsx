fe36671a11e705f9c0000ba6549bb4b9b33c431d
Caio Martins <icaiomarxm@gmail.com>
2024-05-08 04:24:42 -0300

    feat: profile page template

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,5 +1,44 @@
+import { Avatar, AvatarBadge, Box, FormControl, FormLabel, Input, Spinner, VStack } from "@chakra-ui/react";
+import { colors } from "../theme";
+import { Form } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import { EditIcon } from "@chakra-ui/icons";
+import { useEffect, useState } from "react";
+
 export const ProfilePage = () => {
+  const [email, setEmail] = useState("")
+  const [name, setName] = useState("")
+
+  const { user, isLoading } = useAuth()
+
+  useEffect(() => {
+    setEmail(user?.email)
+    setName(user?.full_name)
+  }, [setEmail, setName, user])
+
+  if ( isLoading ) {
+    return <Spinner />
+  }
+
   return (
-    <div>Profile</div>
-  )
-}
+    <Box display="block">
+      <VStack>
+        <Avatar bg={colors.background} size="xl" mb={10} >
+          <AvatarBadge bg="white" >
+            <EditIcon color={colors.background}/>
+          </AvatarBadge>
+        </Avatar>
+        <Form>
+          <FormControl id="email" mb={5}>
+            <FormLabel>Email Address</FormLabel>
+            <Input placeholder={email} variant={"filled"} isDisabled />
+          </FormControl>
+          <FormControl id="name">
+            <FormLabel>Full Name</FormLabel>
+            <Input placeholder={name} variant={"filled"} isDisabled />
+          </FormControl>
+        </Form>
+      </VStack>
+    </Box>
+  );
+};
